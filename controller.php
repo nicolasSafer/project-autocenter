2bd6a286b9cdf9474ca4101458e39f19bd90e431
Nicolas <nicols.safer@gmail.com>
2024-06-07 19:45:01 -0300

    Inicio index

--- controller.php
+++ controller.php
@@ -1,5 +1,5 @@
 <?php
 //a pasta app tarefas está fora do htdocs ou seja fora do online
-require_once 'regras/controller_private.php';
+require_once 'private/controller_private.php';
 
 ?>
--- private/controller_private.php
+++ private/controller_private.php
@@ -1,33 +1,34 @@
 <?php
-require "regras/model.php";
-require "regras/service.php";
-require "regras/conexao.php";
+require "private/model.php";
+require "private/service.php";
+require "private/conexao.php";
 
-if (isset($_GET['acao']) && $_GET['acao'] == 'sucess') {
-    echo '<pre>';
-    print_r($_POST);
-    echo '</pre>';
+//isset($_GET['acao']) && $_GET['acao'] == 'sucess'
+$acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
+
+if ($acao == 'sucess') {
+    // echo '<pre>';
+    // print_r($_POST);
+    // echo '</pre>';
     $login = new Login();
     $conexao = new Conexao();
 
     $login->__set('login', $_POST['email']);
     $login->__set('pswd', $_POST['password']);
 
-    echo '<pre>';
-    print_r($login);
-    echo '</pre>';
+    // echo '<pre>';
+    // print_r($login);
+    // echo '</pre>';
 
     $service = new Acessar($conexao, $login);
     if($service = $service->logar()){
-        header('Location: index.php?acao=sucess');
+        header('Location: index.html?acao=sucess');
     }else{
-        header('Location: login.php?acao=negative');
+        header('Location: login.html?acao=negative');
     }
     
-    // echo '<pre>';
-    // print_r($service);
-    // echo '</pre>';
-    }
+ 
+ }
 
 
 
