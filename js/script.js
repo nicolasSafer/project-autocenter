9ea86c5ae41e83e946c68f9dc0ed6ec4a37370bf
Nicolas <nicols.safer@gmail.com>
2024-06-17 10:27:10 -0300

    Trabalhando com o form

--- js/script.js
+++ js/script.js
@@ -1,18 +1,38 @@
 
+// $(document).ready(function(){
+//     $(".menu > ul > li").click(function(e){
+//         e.preventDefault(); // Evita o comportamento padrão do link, se aplicável
+//         //remobe o active e aplica em outro
+//         $(this).siblings().removeClass("active");
+//         //abre o submenu
+//         $(this).toggleClass("active");
+
+//         $(this).find("ul").slideToggle(); 
+//         //Quando clicar em outro sub menu o anterior vai fechar
+//         $(this).siblings().find("ul").slideUp();
+//         //remove a classe active do item do menu quando clicado no seu submenu
+//         $(this).siblings().find("ul").find("li").removeClass("active");
+//     });
+// });
+
+
 
 $(document).ready(function(){
     $(".menu > ul > li").click(function(e){
-        e.preventDefault(); // Evita o comportamento padrão do link, se aplicável
-        //remobe o active e aplica em outro
+        // Verifica se o item clicado é um link
+        if ($(this).children("a").length === 0) {
+            e.preventDefault(); // Evita o comportamento padrão do link, se aplicável
+        }
+
+        // Remove o active e aplica em outro
         $(this).siblings().removeClass("active");
-        //
+        // Abre o submenu
         $(this).toggleClass("active");
 
         $(this).find("ul").slideToggle(); 
-        //Quando clicar em outro sub menu o anterior vai fechar
+        // Quando clicar em outro sub menu, o anterior vai fechar
         $(this).siblings().find("ul").slideUp();
-        //remove a classe active do item do menu quando clicado no seu submenu
+        // Remove a classe active do item do menu quando clicado no seu submenu
         $(this).siblings().find("ul").find("li").removeClass("active");
     });
 });
-
